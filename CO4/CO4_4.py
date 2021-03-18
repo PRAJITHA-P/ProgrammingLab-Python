82ea31bf55ad39650d9e5e60625830048260c8b1
PRAJITHA P <74781318+PRAJITHA-P@users.noreply.github.com>
2021-03-18 23:39:36 +0530

    course outcome4

--- CO4/CO4_4.py
+++ CO4/CO4_4.py
@@ -1,14 +1,28 @@
 class time:
-    def __init__(self,hour,minute,second):
-        self.__hour=hour
-        self.__minute=minute
-        self.__second=second
+    def __init__(self,h,m,s):
+        self.__h=h
+        self.__m=m
+        self.__s=s
 
-    def __add__(self, other):
-        self.__hour += other.__hour
-        self.__minute +=other.__minute
-        self.__second +=other.__second
+    def __add__(self,obj):
+        self.__h += obj.__h
+        self.__m += obj.__m
+        self.__s += obj.__s
 
-        if (sel.__second>=60):
-            extra_hour=int(self.__minute/60)
-            self.__minute
+        if(self.__s>=60):
+            extra_minute =  int(self.__s/60)
+            self.__s = self.__s%60
+            self.__m += extra_minute
+
+        if (self.__m >= 60):
+            extra_hour = int(self.__m / 60)
+            self.__m = self.__m % 60
+            self.__h += extra_hour
+
+        return "sum of 2 times= "+str(self.__h)+"h:"+str(self.__m)+"m:"+str(self.__s) +"s"
+print("time1=6h:30m:2s")
+print("time2=3h:80m:10s")
+obj1 = time(6,30,2)
+obj2  = time(3,80,10)
+
+print(obj1+obj2)
